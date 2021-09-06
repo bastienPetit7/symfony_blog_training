daca78efe9b47611e3c16484425fe6bc4635cad9
Bastien Petit <bastien.petit77@gmail.com>
2021-09-06 09:27:07 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class); 
+        $articles = $repo->findAll(); 
+
+        // dd($articles); 
+
+        return $this->render('home\index.html.twig', ['articles' => $articles]); 
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show($id): Response
+    {
+
+        $repo = $this->getDoctrine()->getRepository(Article::class); 
+        $article = $repo->find($id); 
+
+        
+        return $this->render('show\index.html.twig', ['article' => $article]);
+    }
+}
